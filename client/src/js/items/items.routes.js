3b0d1241e8161c3fc9ea87e01711086e0e143fce
vzotov <vladimir.zotov@gmail.com>
2015-09-15 16:10:34 +0300

    Added new menu items

--- client/src/js/items/items.routes.js
+++ client/src/js/items/items.routes.js
@@ -2,12 +2,12 @@ var app = require('../app');
 
 app.config(['$stateProvider', '$urlRouterProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-        .state('item/add', {
+        .state('/item/add', {
             url: "/item/add",
             templateUrl: "/partials/items/add.html",
             controller: 'ItemAddController'
         })
-        .state('item/details', {
+        .state('/item/details', {
             url: "/item/:id",
             templateUrl: "/partials/items/details.html",
             controller: 'ItemController'
