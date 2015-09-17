77b560ed1edb0bc31a15d589079bb837bf35bb39
vzotov <vladimir.zotov@gmail.com>
2015-09-17 15:56:57 +0300

    Added swipe for menu toggling

--- client/src/js/main/sidebar.ctrl.js
+++ client/src/js/main/sidebar.ctrl.js
@@ -2,11 +2,13 @@ var app = require('../app');
 
 app.controller('SidemenuController', ['$scope', '$mdUtil', '$mdSidenav', '$rootScope', 'currentUser',
     function ($scope, $mdUtil, $mdSidenav, $rootScope, currentUser) {
-        $rootScope.toggleMenu = $mdUtil.debounce(function () {
-            $mdSidenav('left')
-                .toggle()
+        $rootScope.toggleMenu = $mdUtil.debounce(function (toggle) {
+            var action = angular.isUndefined(toggle) ? 'toggle' : (toggle ? 'open' : 'close'),
+                sideBar = $mdSidenav('left');
+
+            sideBar[action]()
                 .then(function () {
-                    console.log('done!');
+                    console.log(action, 'done!');
                 });
         }, 200);
         $scope.user = currentUser;
