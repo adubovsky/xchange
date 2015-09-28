402856b943d356c6995bbf2c8cbf4a4fba111258
vzotov <vladimir.zotov@gmail.com>
2015-09-28 12:19:24 +0300

    Item-grid directive

---
+++ client/src/js/directives/item-grid.js
@@ -0,0 +1,24 @@
+var app = require('../app');
+
+app.directive('itemGrid', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/partials/directives/item-grid.html',
+        scope: {
+            items: '='
+        },
+        compile: function ($element, $attrs) {
+            var gridEl = $element.children();
+            //Passing md attributes
+            $element.addClass('item-grid-container');
+            angular.forEach($attrs.$attr, function (value, key) {
+                if (key.slice(0, 2) === 'md') {
+                    gridEl.attr(value, $attrs[key]);
+                }
+            });
+        },
+        controller: ['$element',function ($element) {
+
+        }]
+    };
+});
